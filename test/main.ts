fc1173398f06633d277c4e5a4842ddb833653967
Dario Caruso <darioyzf@gmail.com>
2016-08-20 19:22:52 +0200

    to be continued....

--- test/main.ts
+++ test/main.ts
@@ -3,3 +3,19 @@ import watchDog from '../index';
 import chai = require("chai");
 const expect = chai.expect;
 
+
+before(function () {
+
+
+
+
+})
+
+
+describe("XXXXXXXXX", function () {
+
+    it("YYYYYY", function (done) {
+        done()
+    })
+
+})
