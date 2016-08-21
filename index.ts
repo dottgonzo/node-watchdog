2b7711ae1331ec545b6e9ee57226800603c0c167
Dario Caruso <darioyzf@gmail.com>
2016-08-22 01:23:55 +0200

    up

--- index.ts
+++ index.ts
@@ -1,16 +1,28 @@
 import * as Promise from 'bluebird';
 
 
-import * as child_process from 'child_process';
+import * as fs from 'fs';
 
 
 import SysService from 'systemd-services';
 
 
-const fork = child_process.fork;
+
 
 const ns = new SysService()
 
+interface IWatchdogConf {
+    hardware?: string;
+    pings?: string[];
+    interface?: string;
+    file?: string;
+    maxLoad1?: number;
+    maxLoad5?: number;
+    maxLoad15?: number;
+    minMemory?: number;
+    alloctableMemory?: number;
+    configFile?: string;
+}
 
 interface IStatus {
     active: boolean;
@@ -18,9 +30,90 @@ interface IStatus {
 }
 
 export default class watchDog {
+    configFile: string;
+    constructor(o?: IWatchdogConf) {
+        if (o) this.configure(o)
+    }
 
-    constructor() {
+    configure(o: IWatchdogConf) {
+
+        let _this = this;
+
+        if (!o) {
+            throw Error('no configuration provided for watchdog')
+        } else {
+
+            if (!o.configFile) o.configFile = '/etc/watchdog.conf'
+            _this.configFile = o.configFile;
+
+            let conf = 'realtime = yes\n';
+            conf += 'priority = 1\n';
+
+            if (o.pings) {
+                for (let i = 0; i < o.pings.length; i++) {
+                    conf += 'ping = ' + o.pings[i] + '\n'
+                }
+            }
+
+            if (o.hardware) {
+                console.log('TODO')
+            }
+
+            fs.writeFileSync(_this.configFile, conf, { encoding: 'utf-8' })
+
+
+        }
+
+
 
     }
+
+    start() {
+        const _this = this;
+        return new Promise<boolean>((resolve, reject) => {
+            ns.start('watchdog').then((a) => {
+                resolve(a)
+            }).catch((err) => {
+                reject(err)
+            })
+
+        })
+
+    }
+
+
+    stop() {
+        const _this = this;
+        return new Promise<boolean>((resolve, reject) => {
+            ns.stop('watchdog').then((a) => {
+                resolve(a)
+            }).catch((err) => {
+                reject(err)
+            })
+        })
+
+    }
+
+    restart() {
+        const _this = this;
+        return new Promise<boolean>((resolve, reject) => {
+            _this.stop().then(() => {
+                _this.start().then((a) => {
+                    resolve(a)
+                }).catch((err) => {
+                    reject(err)
+                })
+            }).catch((err) => {
+                _this.start().then((a) => {
+                    resolve(a)
+                }).catch((err) => {
+                    reject(err)
+                })
+            })
+
+        })
+
+    }
+
 
 }
--- test/main.ts
+++ test/main.ts
@@ -1,18 +1,34 @@
 import watchDog from '../index';
 
+import * as fs from 'fs';
+
+
 import chai = require("chai");
 const expect = chai.expect;
 
 
+
+const watchConf = {
+    configFile: '/home/dario/watchdog.conf',
+    pings: [
+        '192.168.1.1',
+        '192.168.1.2'
+    ],
+    maxLoad1: 25
+}
+
+let Wdog: watchDog;
+let watchdogconf;
+
 before(() => {
 
-
-
-
+    Wdog = new watchDog(watchConf)
+    watchdogconf = fs.readFileSync(watchConf.configFile, 'utf-8')
+    console.log(watchdogconf)
 })
 
 
-describe("XXXXXXXXX", () => {
+describe("verify configuration", () => {
 
     it("YYYYYY", (done) => {
         done()
